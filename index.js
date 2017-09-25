0d0b239a8e6255231158c85415c1f94cc9badb7c
retotito <retotito@gmail.com>
2017-09-25 10:28:39 +0200

    index message

--- index.js
+++ index.js
@@ -22,7 +22,8 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-  response.render('pages/index');
+  //response.render('pages/index');
+  response.send('Wilkommen');
 });
 
 // middleware for error handling
